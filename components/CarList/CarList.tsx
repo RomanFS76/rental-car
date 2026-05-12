1db639004cb0abacde4b2cf03e61283a5eae81bf
Roman <korvy85@gmail.com>
2026-05-12 22:04:18 +0300

    feat(catalog): prefetch cars with hydration boundary

--- components/CarList/CarList.tsx
+++ components/CarList/CarList.tsx
@@ -1,27 +1,24 @@
 'use client';
 
 import { getCars } from '@/lib/api/api';
-import { keepPreviousData, useQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import css from './CarList.module.css';
 import CarCard from '../CarCard/CarCard';
-
-
 
 const CarList = () => {
   const { data: cars } = useQuery({
     queryKey: ['cars'],
     queryFn: getCars,
-    placeholderData: keepPreviousData,
   });
 
   console.log(cars?.length);
 
   return (
-  <ul className={css.list}>
-    {cars?.map(car => (
-      <CarCard key={car.id} car={car} />
-    ))}
-  </ul>
+    <ul className={css.list}>
+      {cars?.map(car => (
+        <CarCard key={car.id} car={car} />
+      ))}
+    </ul>
   );
 };
 
--- components/Catalog/Catalog.tsx
+++ components/Catalog/Catalog.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import CarList from '../CarList/CarList';
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from '@tanstack/react-query';
+import { getCars } from '@/lib/api/api';
 
-const Catalog = () => {
-  return <CarList />;
+const Catalog = async () => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['cars'],
+    queryFn: getCars,
+  });
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <CarList />
+    </HydrationBoundary>
+  );
 };
 
 export default Catalog;
