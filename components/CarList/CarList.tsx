d5e4c7aa9caffab092f67d127ffded52030cd217
Roman <korvy85@gmail.com>
2026-05-18 20:12:29 +0300

    feat(car-list): add toast notification for empty filter results

--- components/CarList/CarList.tsx
+++ components/CarList/CarList.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { getCars } from '@/lib/api/api';
+import toast from 'react-hot-toast';
 import { useInfiniteQuery, useQueryClient } from '@tanstack/react-query';
 import css from './CarList.module.css';
 import CarCard from '../CarCard/CarCard';
@@ -46,9 +47,15 @@ const CarList = ({ params }: PropsCarList) => {
       },
     });
 
-    (data)
-
   const cars = data?.pages.flatMap(page => page.cars) ?? [];
+
+  const hasFilters = Object.values(params).some(Boolean);
+
+  useEffect(() => {
+    if (!isLoading && data && hasFilters && cars.length === 0) {
+      toast.error('No cars found for your filters');
+    }
+  }, [isLoading, data, hasFilters, cars.length]);
 
   if (isLoading) {
     return <Loader />;
